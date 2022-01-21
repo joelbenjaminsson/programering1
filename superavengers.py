6e156ed928a11d111ded845b9ecd0d75de3f522e
joelbenjaminsson <91466225+joelbenjaminsson@users.noreply.github.com>
2022-01-21 10:32:47 +0100

    Create superavengers.py

---
+++ superavengers.py
@@ -0,0 +1,43 @@
+my_list = ['Superman', 'Spiderman', 'Ironman', 'The Hulk', 'Thor']
+
+# first item
+print(my_list[0])  
+
+# first item
+print(my_list[1])
+# first item
+print(my_list[2])
+
+# third item
+print(my_list[3])  
+
+# fifth item
+print(my_list[4])  
+
+
+my_list = ['Ant-man', 'Batman', 'Superman', 'Deadpool', 'Blackpanther']
+
+# first item
+print(my_list[0])  
+
+# first item
+print(my_list[1])
+# first item
+print(my_list[2])
+
+# third item
+print(my_list[3])  
+
+# fifth item
+print(my_list[4])  
+
+
+import random
+
+secure_random = random.SystemRandom()
+print(secure_random.choice(my_list))
+
+import random
+
+secure_random = random.SystemRandom()
+print(secure_random.choice(my_list))
